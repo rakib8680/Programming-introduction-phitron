50b2fd1a9ebde5822a954eb9e6f14ca7ca7bdd9f
rakib8680 <rakib.cst.1st@gmail.com>
2025-02-11 19:39:36 +0600

    Add special character examples in print.c: newline, tab, backslash, percentage symbol

---
+++ Module-1/print.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main(){
+    printf("My name is Rakib\nI'm a student of CSE\t\n");
+    printf("\\");
+    printf("100%%");
+    printf("\nI'm a programmer");
+    return 0;
+};
+
+
+
+
+
+/*special characters :-
+* \n - new line 
+* \t - 4 space  
+*/
