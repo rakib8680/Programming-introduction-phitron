40061ea56fa55d98c7dabebe81a5d2607270e90e
rakib8680 <rakib.cst.1st@gmail.com>
2025-02-15 18:53:03 +0600

    Learned to use scanf for int, char, float, and percentage inputs

---
+++ Module-1/input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+
+    int rakib;
+    char name;
+    float cgpa;
+    scanf("%d %c %f", &rakib, &name, &cgpa);
+    printf("%d %c %0.2f", rakib, name, cgpa);
+    int a, b;
+    scanf("%d%% %d%%", &a, &b);
+    printf("%d%% %d%%", a, b);
+
+
+    return 0;
+}
