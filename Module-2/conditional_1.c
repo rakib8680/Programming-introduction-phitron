11e2be6205c8fbd84b672dc2198ff79d26ff1160
rakib8680 <rakib.cst.1st@gmail.com>
2025-02-17 18:02:22 +0600

    Learned conditional statement(if..else)

---
+++ Module-2/conditional_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int taka;
+    scanf("%d", &taka);
+    if(taka>=100){
+        printf("You are rich\n");
+    }else{
+        printf("You are poor\n");
+    }
+
+
+    return 0;
+}
