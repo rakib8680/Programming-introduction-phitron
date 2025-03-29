92cf6788f6ca03ffa760007299535ac05de0d504
rakib8680 <rakib.cst.1st@gmail.com>
2025-03-29 16:28:43 +0600

    Added code to insert a value at a specified position in an array and print the updated array

---
+++ Module-9/insert.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Write a program that inserts a given value at a specified position in an array of integers.
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+// 1 100
+
+// Output:
+// 10 100 20 30 40 50
+
+int main()
+{
+    int n, pos, value;
+    scanf("%d", &n);
+    int arr[n + 1];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    };
+    scanf("%d %d", &pos, &value);
+
+    for (int i = n; i >= pos + 1; i--)
+    {
+        arr[i] = arr[i - 1];
+    };
+
+    arr[pos] = value;
+
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
