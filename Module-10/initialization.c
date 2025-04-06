5bddf5d4aa61643de408b764c7af86d1a5e6bf08
rakib8680 <rakib.cst.1st@gmail.com>
2025-04-06 19:49:06 +0600

    Fix string declaration and add size calculation & added notes

--- Module-10/initialization.c
+++ Module-10/initialization.c
@@ -2,12 +2,33 @@
 
 int main()
 {
-    char name[5] = "Rakib";
+    char name[6] = "Rakib";
+    int size = sizeof(name) / sizeof(name[0]); // size of array = total size of array / size of each element
+    // size of array = 6/1 = 6
+    // size of each element = 1 byte (char)
+    // for int size = 4 bytes (int)
+
     for (int i = 0; i < 5; i++)
     {
         printf("%c", name[i]);
     }
-    printf("\n%s", name); // use "%s" inside printf to print array of char/string without loop
+    printf("\n%s\n%d", name, size); // use "%s" inside printf to print array of char/string without loop
 
     return 0;
 }
+
+// notes
+/*
+1.Always put 1 extra size when declaring a string in C. Because C uses null character '\0' to terminate the string.
+2. If you don't put 1 extra size, then it will not print the string properly. It will print garbage value.
+3. In C, string is an array of characters. So, you can use array functions to manipulate strings.
+4. Use "%s" inside printf to print array of char/string without loop.
+5. Declare string like this
+   char name[6] = "Rakib"; // 5 characters + 1 null character
+   or
+   char name[] = "Rakib"; // compiler will automatically allocate size for the string
+   not like this
+    char name[5] = "Rakib"; // this will not work properly because it will not allocate size for null character
+    or
+    char name[6] = {'R', 'a', 'k', 'i', 'b'}; // this will not work properly because it will not allocate size for null character
+*/
