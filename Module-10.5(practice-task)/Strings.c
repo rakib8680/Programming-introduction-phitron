4bde5e1b2811919a54cd0cf4d0c0eb5a521908f1
rakib8680 <rakib.cst.1st@gmail.com>
2025-04-17 21:01:02 +0600

    Added code to concatenate two strings and calculate their lengths

--- Module-10.5(practice-task)/Strings.c
+++ Module-10.5(practice-task)/Strings.c
@@ -34,24 +34,35 @@ int main()
     scanf("%s %s", str1, str2);
     int len1 = strlen(str1), len2 = strlen(str2);
 
+    // Declare a new character array to store the concatenated result of str1 and str2
+    // The size is the sum of the lengths of the two strings plus 1 for the null terminator
     char newStr[len1 + len2 + 1];
+
+    // Copy all characters from str1 into newStr
     for (int i = 0; i < len1; i++)
     {
         newStr[i] = str1[i];
-    };
-    int i = len1;
+    }
+
+    // Continue copying characters from str2 into newStr, starting where str1 ended
+    int i = len1; // Start index for str2 in newStr
     for (int j = 0; j < len2; j++)
     {
         newStr[i] = str2[j];
         i++;
-    };
+    }
+
+    // Add the null terminator to the end of newStr to make it a valid string
     newStr[i] = '\0';
 
+    // Swap the first characters of str1 and str2
     char temp = str1[0];
     str1[0] = str2[0];
     str2[0] = temp;
 
-    printf("%d %d\n%s\n%s %s", len1, len2, newStr, str1, str2);
+    printf("%d %d\n", len1, len2);
+    printf("%s\n", newStr);
+    printf("%s %s", str1, str2);
 
     return 0;
 }
