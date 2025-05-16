49fedea2d61afd4df7ebb8845c37d6c9c3128192
rakib8680 <rakib.cst.1st@gmail.com>
2025-05-16 19:43:13 +0600

    Practice- Add code to count the occurrences of integers in an array and print the frequency of each number

--- Module-11/counting_array_2.c
+++ Module-11/counting_array_2.c
@@ -1,19 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
     int n;
     scanf("%d", &n);
     int arr[n];
-    int count[7] = {0};
-
+    int count[11] = {0};
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
+        // printf("%d ", arr[i]);
 
         count[arr[i]]++;
     };
 
     for (int i = 0; i < n; i++)
     {
-
         printf("%d - %d\n", i, count[i]);
     }
-    // printf("1- %d\n", count[1]);
-    // printf("2- %d\n", count[2]);
+
+    return 0;
+}
